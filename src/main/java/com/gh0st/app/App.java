ae2cb174bdab081ff293d9195f775bd81d22a975
Poyraz Özbeğ <poyrazozbeg@eskisehir.edu.tr>
2022-11-01 00:57:47 +0300

    message

---
+++ src/main/java/com/gh0st/app/App.java
@@ -0,0 +1,13 @@
+package com.gh0st.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello P" );
+    }
+}
